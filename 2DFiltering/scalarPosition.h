0c3e6be85158fd8f19d33a4ce73cf27e9a1b576f
AlanDReeves <alandanielreeves@gmail.com>
2025-10-20 13:00:52 -1000

    Started on 2D filter
    Need to finish conversion of updatePosition function.

---
+++ 2DFiltering/scalarPosition.h
@@ -0,0 +1,24 @@
+#ifndef POSITIONTRACKING
+#define POSITIONTRACKING
+
+/* 
+A class to keep track of the robot's position.
+This version performs Kalman filtering through scalarized calculations
+*/
+
+
+class PositionTracking {
+public:
+    PositionTracking(float posX, float velX, float posXUncert, float velXUncert, float posY, float velY, float posYUncert, float velYUncert, float accelNoise);
+    void updatePosition(float headingDeg, float accel, int dt, float voltage);
+    float voltageToSpeed(float voltage);
+    float getPosX();
+    float getPosY();
+    float getVelX();
+    float getVelY();
+private:
+    float xPosition, xVelocity, xPosUncert, xVelUncert, yPosition, yVelocity, yPosUncert, yVelUncert, accelNoise, xCovariance;
+};
+
+
+#endif
